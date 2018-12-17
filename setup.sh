6bd40c55be12c18e8fc7cb90f56ba96027539d70
Etienne Gramlich <jim_java@gmx.de>
2018-12-17 15:05:40 +0100

    Added todos and numpy for Python 2 and 3

--- setup.sh
+++ setup.sh
@@ -6,17 +6,25 @@
 # - Installs needed packages for the project
 # - Registers services to run at startup
 
+
+
 ## Update repo
 apt-get update
 apt-get upgrade
 
+
 # Install needed packages
-apt-get install python-dev
+apt-get install python-dev python-numpy python3-numpy
+
+
+# ToDo: Clone adafruit repos and install for our wrapper
 
 
 # Set up WiFi-Network AP
 ./WLAN/setup_AccessPoint.sh
 
+
+# Install our server to receive data from smartphone
 
 
 echo "Please reboot your Raspberry Pi!"
