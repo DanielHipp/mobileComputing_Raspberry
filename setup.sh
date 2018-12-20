90e6287423b89323c64e6e18d108c1583672caeb
etgramli <etgramlich@gmx.de>
2018-12-20 11:55:45 +0100

    Enable i2c, added comments

--- setup.sh
+++ setup.sh
@@ -8,13 +8,20 @@
 # - Registers services to run at startup
 
 
+## Enable I²C and add Pi to i2c group to easy usage
+echo "dtparam=i2c1=on" >> /boot/config.txt
+adduser pi i2c
+
 
 ## Update repo
 apt-get update && apt-get upgrade
 
 
 # Install needed packages
+apt-get install i2c-tools
+# Install Python 2 packages
 apt-get install python-dev  python-numpy
+# Install Python 3 packages
 apt-get install python3-dev python3-numpy
 
 
